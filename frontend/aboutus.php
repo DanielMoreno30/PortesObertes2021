2ae4f5a4f923209b60d6633831bad6d3ed6a5687
DanielMoreno2021 <72091490+DanielMoreno2021@users.noreply.github.com>
2021-10-22 13:33:28 +0200

    Navbar arreglada + Footer + About Us

---
+++ frontend/aboutus.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="esp">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portes Obertes</title>
+    <style>
+      body{
+     font-family: "Arial"; 
+     }
+     </style>
+    <link rel="stylesheet" href="../style/bootstrap.min.css">
+    <link rel="icon"  href="../img/politecnicsico.png" type="image/png" />
+    <link rel="stylesheet" href="../style/navbaraboutus.css">
+    <link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
+</head>
+    <!-- -----------------------------------------NAVBAR----------------------------------------------------------------------- -->  
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+        <img src="../img/politecnicsiconsinfondo.png" width="150" height="60" class="d-inline-block align-middle" alt="" loading="lazy" >
+          <a href="" class="navbar-brand">Portes Obertes</a>
+          <div id="main-nav" class="collapse navbar-collapse">
+            <ul class="navbar-nav ml-auto">
+              <li><a href="../index.php" class="nav-item nav-link active">Inicio</a></li>
+              <li><a href="#" class="nav-item nav-link">Cursos</a></li>
+              <li class="dropdown">
+                <a href="#" class="nav-item nav-link" data-toggle="dropdown">Juegos</a>
+                <!-- <div class="dropdown-menu">
+                  <a href="#" class="dropdown-item">Juego 1</a>
+                  <a href="#" class="dropdown-item">Juego 2</a>
+                  <a href="#" class="dropdown-item">Juego 3</a>
+                  <a href="#" class="dropdown-item">Juego 4</a>
+                  <a href="#" class="dropdown-item">Juego 5</a>
+                </div> -->
+              </li>
+              <li class="dropdown">
+                <a href="aboutus.php" class="nav-item nav-link">Sobre Nosotros</a>
+                <!-- <div class="dropdown-menu">
+                  <a href="frontend/aboutus.php" class="dropdown-item">Sebas Angelov</a>
+                  <a href="#" class="dropdown-item">Pablo Ferrer</a>
+                  <a href="#" class="dropdown-item">Óscar Gonzalez</a>
+                  <a href="#" class="dropdown-item">Daniel Moreno</a>
+                  <a href="#" class="dropdown-item">Victor Soriano</a>
+                </div> -->
+              </li>
+              <li><a href="#" class="nav-item nav-link">Contacto</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <section id="content" class="content">
+  <div class="container">
+    <header class="jumbotron my-4">
+	  <center>
+      <h1 style="color: #0089ff;" class="display-3">Portes Obertes</h1>
+      <p class="lead">
+		    Texto a escribir
+	    </p>
+	  </center>
+    </header>
+    <div class="row text-center">
+    <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img" src="" alt="" width="800" height="325" />
+          <div class="card-body">
+            <h4 style="color: #F3B63E;" class="card-title">Daniel Moreno Fernandez</h4>
+            <p class="card-text">Hola</p>
+          </div>
+          <div class="card-footer">
+            <a href="https://www.linkedin.com/in/daniel-moreno-b7603b1b5/" target="_blank" class="btn btn-danger">Linkedin</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img" src="" alt="" width="800" height="325" />
+          <div class="card-body">
+            <h4 style="color: #F3B63E;" class="card-title">Daniel Moreno Fernandez</h4>
+            <p class="card-text">Hola</p>
+          </div>
+          <div class="card-footer">
+            <a href="https://www.linkedin.com/in/daniel-moreno-b7603b1b5/" target="_blank" class="btn btn-danger">Linkedin</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img" src="../img/AvatarDanielMoreno.png" alt="" width="800" height="325" />
+          <div class="card-body">
+            <h4 style="color: #F3B63E;" class="card-title">Daniel Moreno Fernandez</h4>
+            <p class="card-text">Hola</p>
+          </div>
+          <div class="card-footer">
+            <a href="https://www.linkedin.com/in/daniel-moreno-b7603b1b5/" target="_blank" class="btn btn-danger">Linkedin</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img" src="" alt="" width="800" height="325" />
+          <div class="card-body">
+            <h4 style="color: #F3B63E;" class="card-title">Daniel Moreno Fernandez</h4>
+            <p class="card-text">Hola</p>
+          </div>
+          <div class="card-footer">
+            <a href="https://www.linkedin.com/in/daniel-moreno-b7603b1b5/" target="_blank" class="btn btn-danger">Linkedin</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+
+<!-- -----------------------------------------FOOTER----------------------------------------------------------------------- -->
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-dark text-muted">
+  <!-- Section: Social media -->
+  <section
+    class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+  >
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>TEXTO A ESCRIBIR</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <!-- <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+		<i class="fas fa-adjust"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div> -->
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Politècnics en Barcelona
+          </h6>
+          <p>
+          <i class="fas fa-map-marker-alt"></i> Centre Urquinaona <br>
+            Plaça Urquinaona, 10. 08010 Barcelona. <br>
+      <i class="fas fa-phone-alt"></i> 93 302 02 24
+      <i class="fas fa-at"></i> info@politecnics.barcelona
+          </p>
+		  <p>
+		  <i class="fas fa-map-marker-alt"></i> Centre Santa Anna<br>
+            C/Santa Anna, 28. 08002 Barcelona <br>
+      <i class="fas fa-phone-alt"></i> 93 302 41 06 <br>
+      <i class="fas fa-at"></i> info@politecnics.barcelona
+          </p>
+        </div>
+
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Vacio</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vacio</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vacio</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vacio</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Enlaces
+          </h6>
+          <p>
+          <a href="#!" class="text-reset"><i class="fab fa-github"></i> GitHub</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vacio</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vacio</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vacio</a>
+          </p>
+        </div>
+
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contacto
+          </h6>
+          <p><i class="fab fa-linkedin"></i> Sebas Angelov</p>
+          <p><i class="fab fa-linkedin"></i> Pablo Ferrer</p>
+          <p><i class="fab fa-linkedin"></i> Óscar Gonzalez</p>
+          <p><i class="fab fa-linkedin"></i> Daniel Moreno</p>
+		  <p><i class="fab fa-linkedin"></i> Victor Soriano</p>
+        </div>
+      </div>
+    </div>
+	<p class="card-text"></p> Copyright © 2021 - 2022 - Proyecto DAW 2 (Grup 2) - Centre d’Estudis Politècnics</p>
+  </section>
+</footer>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="js/navbar.js"></script>
+  <script src="https://kit.fontawesome.com/668120ee8a.js" crossorigin="anonymous"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-<?php    
-?>
 <!DOCTYPE html>
 <html lang="esp">
 <head>
@@ -31,25 +29,27 @@
 			
 			<div id="main-nav" class="collapse navbar-collapse">
 				<ul class="navbar-nav ml-auto">
-					<li><a href="#" class="nav-item nav-link active">Home</a></li>
+					<li><a href="index.php" class="nav-item nav-link active">Inicio</a></li>
 					<li><a href="#" class="nav-item nav-link">Cursos</a></li>
 					<li class="dropdown">
 						<a href="#" class="nav-item nav-link" data-toggle="dropdown">Juegos</a>
-						<div class="dropdown-menu">
+						<!-- <div class="dropdown-menu">
 							<a href="#" class="dropdown-item">Juego 1</a>
 							<a href="#" class="dropdown-item">Juego 2</a>
 							<a href="#" class="dropdown-item">Juego 3</a>
-						</div>
+							<a href="#" class="dropdown-item">Juego 4</a>
+							<a href="#" class="dropdown-item">Juego 5</a>
+						</div> -->
 					</li>
 					<li class="dropdown">
-						<a href="#" class="nav-item nav-link" data-toggle="dropdown">About Us</a>
-						<div class="dropdown-menu">
-							<a href="#" class="dropdown-item">Sebas Angelov</a>
+						<a href="frontend/aboutus.php" class="nav-item nav-link">Sobre Nosotros</a>
+						<!-- <div class="dropdown-menu">
+							<a href="frontend/aboutus.php" class="dropdown-item">Sebas Angelov</a>
 							<a href="#" class="dropdown-item">Pablo Ferrer</a>
 							<a href="#" class="dropdown-item">Óscar Gonzalez</a>
 							<a href="#" class="dropdown-item">Daniel Moreno</a>
 							<a href="#" class="dropdown-item">Victor Soriano</a>
-						</div>
+						</div> -->
 					</li>
 					<li><a href="#" class="nav-item nav-link">Contacto</a></li>
 				</ul>
@@ -67,7 +67,6 @@
 		</div>
 	</div>
 </header>
-
 <main>
 	<section id="content" class="content">
 		<div class="container">
@@ -96,19 +95,129 @@
 </main>
 
 <!-- -----------------------------------------FOOTER----------------------------------------------------------------------- -->
-  <footer class="bg-dark text-center text-lg-start">
-    <div class="text-center p-3" >
-        <div id="footerLinks">
-            <a href="https://github.com/MrFron/Recomercem" class="badge badge-primary">GitHub</a>
-            <a href="./frontend/about.php" class="badge badge-primary">About</a>
-        </div>
-        <p class="card-text"></p> Copyright © 2021 - 2022 - Proyecto DAW 2B (Grup 2) - Centre d’Estudis Politècnics<br> Sebas Angelov · Pablo Ferrer · Óscar Gonzalez · Daniel Moreno · Victor Soriano</p>
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-dark text-muted">
+  <!-- Section: Social media -->
+  <section
+    class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+  >
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>TEXTO A ESCRIBIR</span>
     </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <!-- <div>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+		<i class="fas fa-adjust"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-linkedin"></i>
+      </a>
+      <a href="" class="me-4 text-reset">
+        <i class="fab fa-github"></i>
+      </a>
+    </div> -->
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Politècnics en Barcelona
+          </h6>
+          <p>
+          <i class="fas fa-map-marker-alt"></i> Centre Urquinaona <br>
+            Plaça Urquinaona, 10. 08010 Barcelona. <br>
+      <i class="fas fa-phone-alt"></i> 93 302 02 24 <br>
+      <i class="fas fa-at"></i> info@politecnics.barcelona
+          </p>
+		  <p>
+		  <i class="fas fa-map-marker-alt"></i> Centre Santa Anna<br>
+            C/Santa Anna, 28. 08002 Barcelona <br>
+      <i class="fas fa-phone-alt"></i> 93 302 41 06 <br>
+      <i class="fas fa-at"></i> info@politecnics.barcelona
+          </p>
+        </div>
+
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Products
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Vacio</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vacio</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vacio</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vacio</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Enlaces
+          </h6>
+          <p>
+          <a href="#!" class="text-reset"><i class="fab fa-github"></i> GitHub</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vacio</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vacio</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Vacio</a>
+          </p>
+        </div>
+
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <h6 class="text-uppercase fw-bold mb-4">
+            Contacto
+          </h6>
+          <p><i class="fab fa-linkedin"></i> Sebas Angelov</p>
+          <p><i class="fab fa-linkedin"></i> Pablo Ferrer</p>
+          <p><i class="fab fa-linkedin"></i> Óscar Gonzalez</p>
+          <p><i class="fab fa-linkedin"></i> Daniel Moreno</p>
+		  <p><i class="fab fa-linkedin"></i> Victor Soriano</p>
+        </div>
+      </div>
+    </div>
+	<p class="card-text"></p> Copyright © 2021 - 2022 - Proyecto DAW 2 (Grup 2) - Centre d’Estudis Politècnics</p>
+  </section>
 </footer>
 
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="js/navbar.js"></script>
+  <script src="https://kit.fontawesome.com/668120ee8a.js" crossorigin="anonymous"></script>
 </body>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-<script type="text/javascript" src="js/navbar.js"></script>
 </html>
