78f38ed7307dff69c2b0b192d3a89f12e3bd5bce
PabloFerrer-examen <pferrer-dalmaua2122@politecnics.barcelona>
2021-12-20 14:33:05 +0100

    Pacmna

--- games/PacmanJuego/pacman2.php
+++ games/PacmanJuego/pacman2.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Pac-man</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style1.css">
     <script src="juego.js" charset="utf-8"></script>
   </head>
   <body>
@@ -15,7 +15,7 @@
     <div class = "opciones">
       <input type="button" value="Restart" onclick="location.reload()"/>
       <input type="button" value="Volver" onclick="location.href= '../../frontend/games.php'"/>
-      <input type="button" value="Cambiar imagenes a Escuela" onclick="location.href='./pacman2.php'"/>
+      <input type="button" value="Cambiar imagenes a Semana Santa" onclick="location.href='./pacman.php'"/>
     </div>
 
   </body>
